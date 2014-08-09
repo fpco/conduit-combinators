dd87814fce350ff9b622b68e9e111468718c7fc9
Mihaly Barasz <klao@nilcons.com>
2014-08-09 20:09:35 +0200

    Fix the benchmark

--- bench/sliding.hs
+++ bench/sliding.hs
@@ -1,5 +1,7 @@
 {-# LANGUAGE TypeFamilies #-}
 {-# LANGUAGE NoMonomorphismRestriction #-}
+{-# LANGUAGE BangPatterns #-}
+
 import Criterion.Main
 import Conduit
 import Data.Conduit.Combinators (slidingWindow, slidingVectorWindow, slidingVectorWindowA)
@@ -7,22 +9,42 @@ import qualified Data.Vector as V
 import qualified Data.Vector.Storable as VS
 import qualified Data.Vector.Unboxed as VU
 import qualified Data.Sequence
-import Data.MonoTraversable (Element)
+import Data.MonoTraversable (Element, unsafeHead)
+import qualified Data.Sequences              as Seq
+import System.Random (randomRIO)
 
-input :: Monad m => Source m Int
-input = yieldMany [1..100000]
+nn, window :: Int
+nn = 20000
+window = 100
 
-benchHelper :: String
-            -> (Int -> Conduit Int IO a)
+input :: Source IO Int
+input = lift (randomRIO (0, nn)) >>= go nn
+  where
+    go !k !x | k < 0     = return ()
+             | otherwise = yield x >> go (k-1) (x+1)
+
+benchHelper :: (Seq.IsSequence seq, Element seq ~ Int)
+            => String
+            -> (Int -> Conduit Int IO seq)
             -> String
-            -> a -- ^ dummy
+            -> seq -- ^ dummy
             -> Benchmark
 benchHelper name1 conduit name2 _dummy =
       bench (concat [name1, ": ", name2])
     $ whnfIO
     $ input
-   $$ conduit 300
-   =$ sinkNull
+   $$ conduit window
+   =$ sinkForce
+
+sinkForce :: (Seq.IsSequence seq, Element seq ~ Int)
+             => Consumer seq IO ()
+sinkForce = go 0
+  where
+    go !x = do
+      ms <- await
+      case ms of
+        Nothing -> return ()
+        Just s -> go (x + unsafeHead s)
 
 {-
 benchV :: (Element (seq Int) ~ Int)
@@ -43,15 +65,15 @@ benchW = benchHelper "slidingWindow" slidingWindow
 
 main :: IO ()
 main = defaultMain
-    [ benchV "boxed" V.empty
+    [ benchV "unboxed" VU.empty
+    , benchVA "unboxed" VU.empty
+    , benchW "unboxed vector" VU.empty
+    , benchV "boxed" V.empty
     , benchVA "boxed" V.empty
     , benchW "boxed vector" V.empty
     , benchV "storable" VS.empty
     , benchVA "storable" VS.empty
     , benchW "storable vector" VS.empty
-    , benchV "unboxed" VU.empty
-    , benchVA "unboxed" VU.empty
-    , benchW "unboxed vector" VU.empty
     , benchW "list" []
     , benchW "Seq" Data.Sequence.empty
     ]
